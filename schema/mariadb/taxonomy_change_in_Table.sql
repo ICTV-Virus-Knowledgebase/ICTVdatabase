cf7054ddf3e51f245300ab339af97306340deaec
mimsLR <lmims52@uab.edu>
2025-11-18 22:19:09 +0000

    Delete all SQL objects inside schema/mariadb - The objects inside schema/mariadb/load will then be moved in their place - The objects inside load were better maintained and we do not need duplicates

--- schema/mariadb/taxonomy_change_in_Table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `taxonomy_change_in` (
-  `change` VARCHAR(10) NOT NULL,
-  `notes` TEXT,
-  PRIMARY KEY (`change`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- schema/mariadb/taxonomy_host_source_Table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `taxonomy_host_source` (
-  `host_source` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`host_source`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
