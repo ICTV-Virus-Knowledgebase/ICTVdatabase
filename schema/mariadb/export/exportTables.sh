91bbd14f302fe39aef467abd1ef28534f43fd0c2
mimsLR <lmims52@uab.edu>
2025-11-19 17:09:59 +0000

    Changed mariadb export from .tsv to .txt - The file is still formatted as .tsv - This needed to be done for historical consistency

--- schema/mariadb/export/exportTables.sh
+++ schema/mariadb/export/exportTables.sh
@@ -27,24 +27,24 @@ taxonomy_node_merge_split="taxonomy_node_merge_split"
 #------------------------------#
 
 # species_isolates
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$species_isolates" ORDER BY isolate_id" > species_isolates.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$species_isolates" ORDER BY isolate_id" > species_isolates.utf8.txt
 # taxonomy_toc
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_toc" ORDER BY msl_release_num" > taxonomy_toc.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_toc" ORDER BY msl_release_num" > taxonomy_toc.utf8.txt
 # taxonomy_node
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_node" ORDER BY taxnode_id" > taxonomy_node.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_node" ORDER BY taxnode_id" > taxonomy_node.utf8.txt
 # taxonomy_level
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_level"" > taxonomy_level.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_level"" > taxonomy_level.utf8.txt
 # taxonomy_molecule
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_molecule"" > taxonomy_molecule.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_molecule"" > taxonomy_molecule.utf8.txt
 # taxonomy_host_source
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_host_source"" > taxonomy_host_source.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_host_source"" > taxonomy_host_source.utf8.txt
 # taxonomy_genome_coverage
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_genome_coverage"" > taxonomy_genome_coverage.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_genome_coverage"" > taxonomy_genome_coverage.utf8.txt
 # taxonomy_change_in
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_change_in"" > taxonomy_change_in.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_change_in"" > taxonomy_change_in.utf8.txt
 # taxonomy_change_out
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_change_out"" > taxonomy_change_out.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_change_out"" > taxonomy_change_out.utf8.txt
 # taxonomy_node_delta
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_node_delta" ORDER BY msl, prev_taxid, new_taxid" > taxonomy_node_delta.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_node_delta" ORDER BY msl, prev_taxid, new_taxid" > taxonomy_node_delta.utf8.txt
 # taxonomy_node_merge_split
-mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_node_merge_split" ORDER BY prev_ictv_id, next_ictv_id" > taxonomy_node_merge_split.utf8.tsv
+mariadb -D "$DATABASE" --default-character-set=utf8mb4 --batch --raw -e "SELECT * FROM "$taxonomy_node_merge_split" ORDER BY prev_ictv_id, next_ictv_id" > taxonomy_node_merge_split.utf8.txt
