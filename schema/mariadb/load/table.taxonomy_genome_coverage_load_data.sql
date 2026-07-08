c2214946bbdc7a5197fc8ba8a81743bcffba0cfa
mimsLR <lmims52@uab.edu>
2026-07-08 17:56:32 +0000

    Data export/import change: escape with \ and NULL is represented as \N - "double ""quotes"" to embed" -> "backslash \"escapes\" to embed" - Cannot escape with " as that would turn NULL into "N, which messes up the load - NULL -> \N - exportTables.sh uses SELECT INTO instead of raw select statement - SELECT INTO must use same options on import and export - All string fields are enclosed with doubles quotes - This fixes data parsing errors when a quoted name appears at the beginning of a field

--- schema/mariadb/load/table.taxonomy_genome_coverage_load_data.sql
+++ schema/mariadb/load/table.taxonomy_genome_coverage_load_data.sql
@@ -3,8 +3,8 @@ LOAD DATA LOCAL INFILE '../../../data/taxonomy_genome_coverage.utf8.txt'
 INTO TABLE taxonomy_genome_coverage
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY '\t'
-OPTIONALLY ENCLOSED BY "'"
-ESCAPED BY ''
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (
